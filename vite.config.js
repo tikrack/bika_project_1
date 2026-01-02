500ca4709161c159f1f47ca7f91e38ce620a119a
tikrack <tikrackcode@gmail.com>
2026-01-02 19:31:13 +0330

    new update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/pages/index.html'),
+                about: resolve(__dirname, 'src/pages/about.html'),
+            },
+        },
+    },
     plugins: [
         tailwindcss(),
     ],
